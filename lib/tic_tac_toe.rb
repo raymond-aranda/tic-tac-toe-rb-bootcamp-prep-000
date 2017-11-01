df09e0efa2cd86095de5a7a94098bf437b81f597
Raymond Aranda <raymond.aranda@gmail.com>
2017-11-01 21:23:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,5 +82,12 @@ def winner(board)
 end
 
 def play(board)
-  
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
